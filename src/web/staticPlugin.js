8d14a57f4d2c4402e869a8242aed251430bfa512
I. Dan Calinescu <idancalinescu@gmail.com>
2020-03-21 19:15:57 +0200

    load the static main module properly

--- src/web/staticPlugin.js
+++ src/web/staticPlugin.js
@@ -1,6 +1,7 @@
 const WebPlugin = require('./webPlugin')
 const requireFromString = require('require-from-string')
 const ejs = require('ejs')
+const jsdom = require('jsdom') 
 
 class Plugin extends WebPlugin {
 
@@ -17,18 +18,18 @@ class Plugin extends WebPlugin {
     const bundle = compilation.assets[`${this.context.name}.js`]
     const source = bundle.source()
     // const header = `var self = {}; global.document = { querySelector: Function.prototype }; global.window = {}; global.window.navigator = {};`
-    const header = `
-    const jsdom = require("jsdom"); 
-    const { JSDOM } = jsdom; 
-    const __DOM = new JSDOM("<!DOCTYPE html><div/>");
-    const { window } = __DOM;
-    const { document } = window;
-    `
-    
+
     try {
+      const { JSDOM } = jsdom
+      const __DOM = new JSDOM("<!DOCTYPE html><div/>")
+      const { window } = __DOM
+      global.window = __DOM.window
+      global.document = window.document
+      const header = `global.__DOM = true`
       this._mainModule = requireFromString(`${header}; ${source}`)
       return this._mainModule
     } catch (e) {
+      console.log(e)
     }
   }
 
